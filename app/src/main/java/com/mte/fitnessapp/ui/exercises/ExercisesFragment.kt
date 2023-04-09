f033462e40bfa23d55ccaae8680d0fe04b3ebd6a
mtalhaergun <mtalhaergun24@gmail.com>
2023-04-09 03:42:04 +0300

    Create SearchView for exercises

--- app/src/main/java/com/mte/fitnessapp/ui/exercises/ExercisesFragment.kt
+++ app/src/main/java/com/mte/fitnessapp/ui/exercises/ExercisesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
@@ -16,6 +17,8 @@ import com.mte.fitnessapp.ui.exercises.adapters.ExerciseAdapter
 import com.mte.fitnessapp.ui.exercises.listeners.CategoryClickListener
 import com.mte.fitnessapp.ui.exercises.listeners.ExerciseClickListener
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+import kotlin.collections.ArrayList
 
 @AndroidEntryPoint
 class ExercisesFragment : Fragment() {
@@ -26,10 +29,12 @@ class ExercisesFragment : Fragment() {
     private lateinit var adapterExercise : ExerciseAdapter
     private var categories = arrayListOf<String>()
     private var exercises = listOf<ExercisesItem>()
+    private var tempExercises = listOf<ExercisesItem>()
+    private var firstOpen = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel.getExercises()
+
     }
 
     override fun onCreateView(
@@ -42,15 +47,26 @@ class ExercisesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        createRv()
-        observeEvents()
+        if(firstOpen){
+            viewModel.getExercises()
+            createRv()
+            observeEvents()
+        }else{
+            binding.categoryRv.adapter = adapterCategory
+            binding.exerciseRv.adapter = adapterExercise
+        }
+        listeners()
+
     }
 
     fun observeEvents() {
+
         viewModel.exercisesResponse.observe(viewLifecycleOwner, Observer {
             exercises = it
             adapterExercise.setExercises(exercises)
         })
+
+        firstOpen = false
     }
 
     fun createRv(){
@@ -64,8 +80,10 @@ class ExercisesFragment : Fragment() {
                         }
                     }
                     adapterExercise.setExercises(listExercise)
+                    tempExercises = listExercise
                 }else{
                     adapterExercise.setExercises(exercises)
+                    tempExercises = exercises
                 }
             }
         })
@@ -87,7 +105,43 @@ class ExercisesFragment : Fragment() {
             }
         })
         binding.exerciseRv.adapter = adapterExercise
-
     }
 
+    fun listeners(){
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if(newText != null && newText != ""){
+                    binding.categoryRv.visibility = View.GONE
+                    val filteredList = ArrayList<ExercisesItem>()
+                    for(i in exercises){
+                        if(i.name.lowercase(Locale.ROOT).contains(newText)){
+                            filteredList.add(i)
+                        }
+                    }
+
+                    if(filteredList.isNotEmpty()){
+                        adapterExercise.setExercises(filteredList)
+                    }
+                }else{
+                    binding.categoryRv.visibility = View.VISIBLE
+                    adapterExercise.setExercises(tempExercises)
+                }
+                return true
+            }
+        })
+
+
+//        binding.searchView.setOnCloseListener {
+//            binding.categoryRv.visibility = View.VISIBLE
+//            true
+//        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
