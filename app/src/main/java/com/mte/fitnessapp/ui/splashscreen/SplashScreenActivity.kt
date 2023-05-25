83623a9b895f0d9ae9bb5f174f1d649ca73560fb
mtalhaergun <mtalhaergun24@gmail.com>
2023-05-25 19:04:34 +0300

    Intro to Login create SharedPreferences

--- app/src/main/java/com/mte/fitnessapp/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/mte/fitnessapp/ui/splashscreen/SplashScreenActivity.kt
@@ -1,14 +1,18 @@
 package com.mte.fitnessapp.ui.splashscreen
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mte.fitnessapp.databinding.ActivitySplashScreenBinding
+import com.mte.fitnessapp.ui.authentication.LoginActivity
 import com.mte.fitnessapp.ui.splashscreen.intro.IntroActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashScreenBinding
+    private lateinit var introFirstOpen : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,14 +21,22 @@ class SplashScreenActivity : AppCompatActivity() {
         if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
-
+        introFirstOpen = this.getSharedPreferences("IntroFirstOpen", Context.MODE_PRIVATE)!!
         binding.apply {
             splashScreenLogo.alpha = 0f
             splashScreenLogo.animate().setDuration(1500).alpha(1f).withEndAction{
-                val intent = Intent(this@SplashScreenActivity, IntroActivity::class.java)
-                startActivity(intent)
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-                finish()
+                if(introFirstOpen.getBoolean("introfirstopen",true)){
+                    val intent = Intent(this@SplashScreenActivity, IntroActivity::class.java)
+                    startActivity(intent)
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                    finish()
+                }else{
+                    val intent = Intent(this@SplashScreenActivity, LoginActivity::class.java)
+                    startActivity(intent)
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                    finish()
+                }
+
             }
         }
 
--- app/src/main/java/com/mte/fitnessapp/ui/splashscreen/intro/IntroActivity.kt
+++ app/src/main/java/com/mte/fitnessapp/ui/splashscreen/intro/IntroActivity.kt
@@ -1,6 +1,8 @@
 package com.mte.fitnessapp.ui.splashscreen.intro
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mte.fitnessapp.ui.authentication.LoginActivity
@@ -9,6 +11,7 @@ import com.mte.fitnessapp.databinding.ActivityIntroBinding
 class IntroActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityIntroBinding
+    private lateinit var introFirstOpen : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,8 @@ class IntroActivity : AppCompatActivity() {
             supportActionBar!!.hide()
         }
 
+        introFirstOpen = this.getSharedPreferences("IntroFirstOpen", Context.MODE_PRIVATE)!!
+
         val adapter = ViewPagerAdapter(this)
         adapter.addFragment(Intro1Fragment())
         adapter.addFragment(Intro2Fragment())
@@ -27,6 +32,7 @@ class IntroActivity : AppCompatActivity() {
         binding.indicator.setViewPager2(binding.viewpager)
 
         binding.getStartedBtn.setOnClickListener{
+            introFirstOpen.edit().putBoolean("introfirstopen", false).apply()
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
