52e99ae28fcc51c09bf027caf0d6dac10a22a74d
Emre Özel <emreozel.36@hotmail.com>
2023-06-04 13:58:03 +0300

    add photos like

--- app/src/main/java/com/mte/fitnessapp/ui/home/social/SocialFragment.kt
+++ app/src/main/java/com/mte/fitnessapp/ui/home/social/SocialFragment.kt
@@ -2,6 +2,7 @@ package com.mte.fitnessapp.ui.home.social
 
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -94,6 +95,14 @@ class SocialFragment : Fragment() {
         binding.rVPost.setHasFixedSize(true)
         recyclerViewAdapter= PostAdapter(list,requireContext())
         binding.rVPost.adapter=recyclerViewAdapter
+        binding.socialShimmerInclude.cardListShimmer.startShimmer()
+
+        Handler().postDelayed({
+            binding.rVPost.visibility = View.VISIBLE
+            binding.socialShimmerInclude.root.visibility = View.GONE
+            binding.socialShimmerInclude.cardListShimmer.stopShimmer()
+
+        },1500)
     }
 
 }
